06d6207fb45753aa91b722021dfda44cdbe45604
Naxum <jake@naxite.com>
2017-07-29 14:19:29 -0500

    Make sure popovers can be scrolled, and main body wont scroll while reading a page

--- app/routes/page.js
+++ app/routes/page.js
@@ -1,10 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+    classNames: ['page'],
     // portfolio: Ember.inject.service(),
     model(params) {
         // return portfolio.projects.find((p) => p.id == id);
-        console.log("Params: ", params);
         return Ember.getOwner(this).lookup('service:portfolio').projects.find((p) => p.id == params.id);
     }
 });
